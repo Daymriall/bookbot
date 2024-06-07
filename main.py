0380c19ac9a2970ab751fa0132ad047463c6dce9
Daymriall <Daymriall@gmail.com>
2024-06-07 11:31:15 -0400

    This should be the final result

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ def main(): # master list making sure all items are loaded
     num_words = countWords(text) # count the strings
     count = countLetters(text) # dict character : # of times
     report = only_letters(count) # list letters only
-    sorted = sorting(report,count)
-    organized = organize(sorted)
-    print(f"--- Begin report of{toBook} ---")
+    sorted = sorting(report,count) # making a list of dictionaries
+    print(f"--- Begin report of {toBook} ---")
     print(f"{num_words} words found in the docutment")
-    print(organized)
+    print("")
+    def sort_on(dict):
+        return dict["num"]
+    sorted.sort(reverse=True, key=sort_on)
+    for each in sorted:
+        print(f"The '{each['Letter']}' character was found {each['num']} times")
+
+
+
+
+    
+            
+
 
 def countWords(text): # to count the words
     words = text.split() # split the book into words
@@ -18,7 +29,7 @@ def get_book_text(toBook): # to pull the book into here
     with open(toBook) as f: # to make opening the book f
         return f.read() # to read f
 
-def countLetters(text): #pull book
+def countLetters(text): #pull book becomes count
     counting = {}
     for letters in text: # break book to letters
         lowered = letters.lower()
@@ -28,7 +39,7 @@ def countLetters(text): #pull book
             counting[lowered] = 1
     return counting
 
-def only_letters(count):
+def only_letters(count): # becomes report
     is_letter = []
     isnot_letter = []
     for each_in_list in count:
@@ -38,20 +49,22 @@ def only_letters(count):
             isnot_letter.append(each_in_list)
     return is_letter
     
-def sorting(report,count):
+def sorting(report,count): # becomes sorted (Need to assign letter and number both to key pair)
     fin_ito = {}
     list_of_dicts = []
     for i in range(len(report)):
         if report[i] in count:
-            fin_ito[report[i]] = count[report[i]]
+            fin_ito['Letter'] = report[i]
+            fin_ito.update({"num": count[report[i]]})
             list_of_dicts.append(fin_ito)
             fin_ito = {}
     return list_of_dicts
 
-def organize(sorted):
-    return sorted[0]
-sorted.sort(reverse=True,key=organize)
-print(sorted)
+def sort_on(sorted):
+    return sorted["num"]
+
+
+
 
 
 
